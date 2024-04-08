801df5d24295ea68c3cd5e6fee67f5ef9eadf903
RocioAV <44351449@fi.unju.edu.ar>
2024-04-08 19:16:48 -0300

    Resolucion ejercicio1 - promedio de 5 numeros

--- tp1_RocioVelazquez/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_RocioVelazquez/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -4,7 +4,16 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int n1, n2, n3, n4, n5;
+		n1 = 32;
+		n2=65;
+		n3=75;
+		n4=12;
+		n5=88;
 		
+		double promedio= (n1+n2+n3+n4+n5)/ 5.0;
+		
+		System.out.println("El proimedio es: " + promedio);
 	}
 
 }
