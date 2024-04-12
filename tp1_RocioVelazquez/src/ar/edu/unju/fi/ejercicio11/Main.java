1f9ba9a412788ed02e1f492ad832229250e27dff
RocioAV <44351449@fi.unju.edu.ar>
2024-04-12 18:25:34 -0300

    Resolucion ejericicio1 - uso del for

---
+++ tp1_RocioVelazquez/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,12 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for (int i = 2,j=40 ; j > 0 ; i++,j--) {
+			System.out.print(i*j + " ");
+		}
+	}
+
+}
